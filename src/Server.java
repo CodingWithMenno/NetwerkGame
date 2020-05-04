3060f206c5fc0c6f0581c42bd0f145b22859319d
Menno <my.bil@student.avans.nl>
2020-05-04 11:40:37 +0200

    [feature] server class start

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,38 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Server {
 
+    private int port;
+    private ServerSocket serverSocket;
+
     public static void main(String[] args) {
-        System.out.println("test");
+        Server server = new Server(500);
+        server.connect();
+        server.close();
+    }
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public void connect() {
+        if (this.serverSocket.isClosed()) {
+            try {
+                this.serverSocket = new ServerSocket(this.port);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void close() {
+        if (!this.serverSocket.isClosed()) {
+            try {
+                this.serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
